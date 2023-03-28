8625bf9567b6c965b9be23cb4a65a1c94a024200
Haruka Fukukawa <fsharuka@gmail.com>
2023-03-28 21:46:04 +0900

    Adds line width changes

--- simple_line_sequences.py
+++ simple_line_sequences.py
@@ -3,11 +3,13 @@ img_width = 256
 size(256, 256)
 background(0)
 stroke(255)
-strokeWeight(3)
 
-dir_path = "/tmp/simple_line" 
+dir_path = "/tmp/simple_line" # Make sure these directories are created in advance
 seq_length = 10 # number of images in a sequence
 num_seqs = 500 # number of total sequences
+line_widens = False
+if line_widens:
+    dir_path = "/tmp/simple_line_widens" 
 
 for j in range(0, num_seqs):  # Start line at random place and grow longer from both ends
     start_x = random(img_width)
@@ -15,15 +17,21 @@ for j in range(0, num_seqs):  # Start line at random place and grow longer from 
     traj_x = int(random(-10, 10))
     traj_y = int(random(-10, 10))
     background(0)
-
+    stroke_weight = random(0, 10)
+    strokeWeight(stroke_weight)
     magnitude = int(random(1, 10)) # Determines length of initial line
     endpt_x = [start_x, start_x + traj_x * magnitude]
     endpt_y = [start_y, start_y + traj_y * magnitude]
     line(endpt_x[0], endpt_y[0] , endpt_x[1], endpt_y[1])
     magnitude = int(random(1, 5)) # Determines magnitude of line growth
+    line_width_magnitude = random(0, 1)
 
     for i in range(0, seq_length):
+        background(0)
         endpt_x = [endpt_x[0] - traj_x * magnitude, endpt_x[1] + traj_x * magnitude]
         endpt_y = [endpt_y[0] - traj_y * magnitude,  endpt_y[1] + traj_y * magnitude]
+        if line_widens:
+            stroke_weight += line_width_magnitude
+            strokeWeight(stroke_weight)
         line(endpt_x[0], endpt_y[0], endpt_x[1], endpt_y[1])
         save("{}/{}_{}.png".format(dir_path,j,i))
